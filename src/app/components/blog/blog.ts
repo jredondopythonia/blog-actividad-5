c2d2289d567fe0256dbfcd680e2a9ed6feeb2c5d
Jose Redondo Reina <jredondopythonia@gmail.com>
2026-02-21 19:51:47 +0100

    El funcionamiento basico se cumple

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -1,26 +1,55 @@
 import { Component } from '@angular/core';
-import {Post} from '../../interafaces/post'
+import { Post } from '../../interafaces/post'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.html',
   styleUrl: './blog.css',
 })
 export class Blog {
   posts: Post[] = [
     {
-      title:'Primer noticia',
+      title: 'Primer noticia',
       imageUrl: 'https://picsum.photos/401',
       body: 'Cuerpo de la primera noticia',
       date: '2026-02-21'
     },
     {
-      title:'Segunda noticia',
+      title: 'Segunda noticia',
       imageUrl: 'https://picsum.photos/402',
       body: 'Cuerpo de la segunda noticia',
       date: '2026-02-20'
     }
 
   ]
+
+  errorMessage: string = '';
+
+  newPost: Post = {
+    title: "",
+    imageUrl: "",
+    body: "",
+    date: "",
+  }
+
+  addPost() {
+
+    if (
+      !this.newPost.title.trim() ||
+      !this.newPost.imageUrl.trim() ||
+      !this.newPost.body.trim() ||
+      !this.newPost.date.trim()
+    ) {
+      this.errorMessage = 'Todos los campos son obligatorios';
+      return;
+    }
+    console.log(this.newPost);
+
+    this.posts.unshift({ ...this.newPost });
+
+
+
+  }
 }
