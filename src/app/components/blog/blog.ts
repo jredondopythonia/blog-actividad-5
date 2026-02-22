08d4659567dd48277337c6b87f70ba415db565b9
Jose Redondo Reina <jredondopythonia@gmail.com>
2026-02-22 12:56:18 +0100

    Funcionando y con mensaje de error, guardo para realizar mejoras

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -9,21 +9,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.css',
 })
 export class Blog {
+
   posts: Post[] = [
     {
-      title: 'Primer noticia',
-      imageUrl: 'https://picsum.photos/401',
-      body: 'Cuerpo de la primera noticia',
-      date: '2026-02-21'
+      title: 'La inteligencia artificial redefine el mercado laboral en 2026',
+      imageUrl: "https://elordenmundial.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2023/04/que-es-inteligencia-artificial.jpg.webp",
+      body: 'La adopción masiva de herramientas basadas en inteligencia artificial está transformando múltiples sectores, desde la educación hasta la industria tecnológica. Las empresas están invirtiendo cada vez más en automatización y análisis predictivo, lo que está generando nuevas oportunidades laborales pero también exigiendo una rápida adaptación por parte de los profesionales. La formación continua y el desarrollo de habilidades digitales se han convertido en factores clave para mantenerse competitivo en el mercado actual.',
+      date: '2026-02-10'
     },
     {
-      title: 'Segunda noticia',
-      imageUrl: 'https://picsum.photos/402',
-      body: 'Cuerpo de la segunda noticia',
-      date: '2026-02-20'
+      title: 'El teletrabajo híbrido se consolida como modelo dominante',
+      imageUrl: 'https://i.blogs.es/54fe17/kristin-wilson-z3htkdhuh5w-unsplash/1366_2000.jpg',
+      body: 'Después de varios años de transformación en los modelos laborales, el trabajo híbrido se ha establecido como la opción preferida por muchas organizaciones. Este sistema combina la flexibilidad del trabajo remoto con la colaboración presencial, mejorando la productividad y el bienestar de los empleados. Expertos en recursos humanos destacan que las empresas que adoptan políticas flexibles suelen experimentar mayor retención de talento y mejores resultados a largo plazo.',
+      date: '2026-02-05'
     }
-
-  ]
+  ];
 
   errorMessage: string = '';
 
@@ -34,8 +34,21 @@ export class Blog {
     date: "",
   }
 
+  resetnewPost() {
+    this.newPost = {
+      title: "",
+      imageUrl: "",
+      body: "",
+      date: "",
+    }
+  }
+
+
+
+
   addPost() {
 
+    this.errorMessage = "";
     if (
       !this.newPost.title.trim() ||
       !this.newPost.imageUrl.trim() ||
@@ -43,13 +56,18 @@ export class Blog {
       !this.newPost.date.trim()
     ) {
       this.errorMessage = 'Todos los campos son obligatorios';
+
+
       return;
     }
     console.log(this.newPost);
 
     this.posts.unshift({ ...this.newPost });
+    this.errorMessage = '';
 
+    this.resetnewPost()
 
 
   }
+
 }
